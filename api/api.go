2711a643f7f09abbc25af80a756b39c1b3c93fb6
Jeeyoung Kim <jee@squareup.com>
2015-04-07 14:03:56 -0700

    Updating readme & additional commits

--- api/api.go
+++ api/api.go
@@ -1,5 +1,35 @@
 package api
 
-func Test() string {
-  return "test"
+type MetricKey string
+
+type TagSet map[string]string
+
+type TaggedMetric struct {
+	MetricKey MetricKey
+	TagSet    TagSet
+}
+
+type GraphiteMetric string
+
+type Api struct {
+}
+
+func (api *Api) AddMetric(metric TaggedMetric) error {
+  return nil
+}
+
+func (api *Api) ToGraphiteName(metric TaggedMetric) GraphiteMetric {
+  return ""
+}
+
+func (api *Api) ToTaggedName(metric GraphiteMetric) TaggedMetric {
+  return TaggedMetric{}
+}
+
+func (api *Api) GetAllTags(metricKey MetricKey) []TagSet {
+  return nil
+}
+
+func (api *Api) GetMericsForTag(tagKey string, tagValue string) []MetricKey {
+  return nil
 }
