7772afb4593b2e878a1db5d0e419ee4a94821355
Jeeyoung Kim <jee@squareup.com>
2015-04-09 18:52:03 -0700

    Adding YAML file

--- internal/rules.go
+++ internal/rules.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"errors"
 	"regexp"
-	"square/vis/metrics-indexer/api"
 	"strings"
+
+	"gopkg.in/yaml.v2"
+	"square/vis/metrics-indexer/api"
 )
 
 var defaultRegex = "[^.]+"
@@ -24,28 +26,39 @@ type RawRule struct {
 	Regex     map[string]string `yaml:"regex"`
 }
 
-// CompiledRule is sanitized version of RawRule. Only valid rules
-// can be converted to CompiledRule.
-type CompiledRule struct {
+// RawRules is list of RawRule
+type RawRules struct {
+	RawRules []RawRule `yaml:"rule"`
+}
+
+// Rule is a sanitized version of RawRule. Only valid rules
+// can be converted to Rule.
+type Rule struct {
 	rule  RawRule
 	regex *regexp.Regexp
 	tags  []string
 }
 
+// RuleSet is a sanitized version of RawRules.
+// Rules are matched sequentially until a correct one is matched.
+type RuleSet struct {
+	rules []Rule
+}
+
 // Compile a given RawRule into a regex and exposed tagset.
-func Compile(rule RawRule) (CompiledRule, error) {
+func Compile(rule RawRule) (Rule, error) {
 	if len(rule.MetricKey) == 0 {
-		return CompiledRule{}, ErrInvalidMetricKey
+		return Rule{}, ErrInvalidMetricKey
 	}
 	tags := rule.extractTags()
 	if tags == nil {
-		return CompiledRule{}, ErrInvalidPattern
+		return Rule{}, ErrInvalidPattern
 	}
 	regex := rule.toRegexp()
 	if regex == nil {
-		return CompiledRule{}, ErrInvalidPattern
+		return Rule{}, ErrInvalidPattern
 	}
-	return CompiledRule{
+	return Rule{
 		rule,
 		regex,
 		tags,
@@ -53,8 +66,8 @@ func Compile(rule RawRule) (CompiledRule, error) {
 }
 
 // MatchRule sees if a given graphite string matches the rule, and if so, returns the generated tag.
-func (compiledRule CompiledRule) MatchRule(input string) (api.TaggedMetric, bool) {
-	matches := compiledRule.regex.FindStringSubmatch(input)
+func (rule Rule) MatchRule(input string) (api.TaggedMetric, bool) {
+	matches := rule.regex.FindStringSubmatch(input)
 	if matches == nil {
 		return api.TaggedMetric{}, false
 	}
@@ -63,13 +76,25 @@ func (compiledRule CompiledRule) MatchRule(input string) (api.TaggedMetric, bool
 		if index == 0 {
 			continue
 		}
-		tagKey := compiledRule.tags[index-1]
+		tagKey := rule.tags[index-1]
 		tagSet[tagKey] = tagValue
 	}
 	return api.TaggedMetric{
-		compiledRule.rule.MetricKey,
+		rule.rule.MetricKey,
 		tagSet,
 	}, true
+}
+
+// MatchRule sees if a given graphite string matches
+// any of the specified rules.
+func (ruleSet RuleSet) MatchRule(input string) (api.TaggedMetric, bool) {
+	for _, rule := range ruleSet.rules {
+		value, matched := rule.MatchRule(input)
+		if matched {
+			return value, matched
+		}
+	}
+	return api.TaggedMetric{}, false
 }
 
 func (rule RawRule) toRegexp() *regexp.Regexp {
@@ -130,6 +155,23 @@ func (rule RawRule) extractTags() []string {
 		exists[token] = true
 	}
 	return result
+}
+
+// LoadYAML loads a RuleSet from the byte array of the YAML file.
+func LoadYAML(input []byte) (RuleSet, error) {
+	rawRules := RawRules{}
+	if err := yaml.Unmarshal(input, &rawRules); err != nil {
+		return RuleSet{}, err
+	}
+	rules := make([]Rule, len(rawRules.RawRules))
+	for index, rawRule := range rawRules.RawRules {
+		if rule, err := Compile(rawRule); err != nil {
+			return RuleSet{}, err
+		} else {
+			rules[index] = rule
+		}
+	}
+	return RuleSet{rules}, nil
 }
 
 func isTagPortion(index int) bool {
