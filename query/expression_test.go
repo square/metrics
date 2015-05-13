29d2cd0f7f997ec0b2d62a6e67eaba73224806e9
Alex Chow <chow@squareup.com>
2015-05-13 15:14:45 -0700

    Implement tests for evaluateBinaryOperation

--- query/expression_test.go
+++ query/expression_test.go
@@ -31,6 +31,17 @@ func (b FakeBackend) FetchSeries(query api.Query) api.SeriesList {
 	return api.SeriesList{}
 }
 
+type LiteralExpression struct {
+	Values []float64
+}
+
+func (expr *LiteralExpression) Evaluate(context EvaluationContext) (*api.SeriesList, error) {
+	return &api.SeriesList{
+		[]api.Timeseries{api.Timeseries{expr.Values, api.TaggedMetric{}}},
+		api.Timerange{},
+	}, nil
+}
+
 func Test_ScalarExpression(t *testing.T) {
 	for _, test := range []struct {
 		expectSuccess  bool
@@ -75,15 +86,63 @@ func Test_ScalarExpression(t *testing.T) {
 }
 
 func Test_evaluateBinaryOperation(t *testing.T) {
+	emptyContext := EvaluationContext{FakeBackend{}, api.Timerange{}}
 	for _, test := range []struct {
-		expectSuccess bool
-		context       EvaluationContext
-		functionName  string
-		operands      []Expression
-		evalFunction  func(float64, float64) float64
-	}{} {
-		// XXX Add tests
-		_ = test
+		context              EvaluationContext
+		functionName         string
+		operands             []Expression
+		evalFunction         func(float64, float64) float64
+		expectSuccess        bool
+		expectedResultValues []float64
+	}{
+		{
+			emptyContext,
+			"add",
+			[]Expression{
+				&LiteralExpression{
+					[]float64{1, 2, 3},
+				},
+				&LiteralExpression{
+					[]float64{4, 5, 1},
+				},
+			},
+			func(left, right float64) float64 { return left + right },
+			true,
+			[]float64{5, 7, 4},
+		},
+		{
+			emptyContext,
+			"subtract",
+			[]Expression{
+				&LiteralExpression{
+					[]float64{1, 2, 3},
+				},
+				&LiteralExpression{
+					[]float64{4, 5, 1},
+				},
+			},
+			func(left, right float64) float64 { return left - right },
+			true,
+			[]float64{-3, -3, 2},
+		},
+	} {
+		a := assert.New(t).Contextf("%+v", test)
+
+		result, err := evaluateBinaryOperation(
+			test.context,
+			test.functionName,
+			test.operands,
+			test.evalFunction,
+		)
+
+		a.EqBool(err == nil, test.expectSuccess)
+		// Nothing else to validate if we expect failure
+		if !test.expectSuccess {
+			continue
+		}
+
+		a.EqInt(len(result.Series), 1)
+		a.Eq(result.Series[0].Values, test.expectedResultValues)
 	}
 }
 
