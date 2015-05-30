4b2d6f49416ea1a87032af8a5337cde02d5be433
Nathan Fenner <nathanf@squareup.com>
2015-05-29 17:17:46 -0700

    add tests for overall aggregateBy in aggregate_test.go

--- query/aggregate_test.go
+++ query/aggregate_test.go
@@ -191,6 +191,8 @@ var aggregationTestCases = []struct {
 	},
 }
 
+const epsilon = 1e-10 // epsilon is a constant for the maximum allowable error between correct test case answers and actual results
+
 func Test_applyAggregation(t *testing.T) {
 	for _, testCase := range aggregationTestCases {
 		result, err := applyAggregation(testGroup, testCase.Aggregator)
@@ -205,9 +207,223 @@ func Test_applyAggregation(t *testing.T) {
 		}
 		// Next, compare the aggregated values:
 		for i, correct := range testCase.Expected {
-			if math.Abs(result.Values[i]-correct) > 1e-10 {
+			if math.Abs(result.Values[i]-correct) > epsilon {
 				t.Fatalf("applyAggregation() produces incorrect values on aggregation %+v; should be %+v but is %+v", testCase.Aggregator, testCase.Expected, result.Values)
 			}
 		}
 	}
+}
+
+var testList = api.SeriesList{
+	[]api.Timeseries{
+		api.Timeseries{
+			Values: []float64{0, 1, 2},
+			TagSet: api.TagSet{
+				"env":  "staging",
+				"dc":   "A",
+				"host": "q77",
+			},
+		},
+		api.Timeseries{
+			Values: []float64{4, 4, 4},
+			TagSet: api.TagSet{
+				"env":  "staging",
+				"dc":   "B",
+				"host": "r53",
+			},
+		},
+		api.Timeseries{
+			Values: []float64{-1, -1, 2},
+			TagSet: api.TagSet{
+				"env":  "production",
+				"dc":   "A",
+				"host": "y1",
+			},
+		},
+		api.Timeseries{
+			Values: []float64{0, 2, 0},
+			TagSet: api.TagSet{
+				"env":  "production",
+				"dc":   "A",
+				"host": "w20",
+			},
+		},
+		api.Timeseries{
+			Values: []float64{2, 0, 0},
+			TagSet: api.TagSet{
+				"env":  "production",
+				"dc":   "B",
+				"host": "t8",
+			},
+		},
+		api.Timeseries{
+			Values: []float64{0, 0, 1},
+			TagSet: api.TagSet{
+				"env":  "production",
+				"dc":   "C",
+				"host": "b38",
+			},
+		},
+	},
+	api.Timerange{
+		40,
+		280,
+		6,
+	},
+	"Test.List",
+}
+
+var aggregatedTests = []struct {
+	Tags       []string
+	Aggregator aggregator
+	Results    []api.Timeseries
+}{
+	{
+		[]string{"env"},
+		sumAggregator{},
+		[]api.Timeseries{
+			api.Timeseries{
+				Values: []float64{1, 1, 3},
+				TagSet: map[string]string{
+					"env": "production",
+				},
+			},
+			api.Timeseries{
+				Values: []float64{4, 5, 6},
+				TagSet: map[string]string{
+					"env": "staging",
+				},
+			},
+		},
+	},
+	{
+		[]string{"dc"},
+		maxAggregator{},
+		[]api.Timeseries{
+			api.Timeseries{
+				Values: []float64{0, 2, 2},
+				TagSet: map[string]string{
+					"dc": "A",
+				},
+			},
+			api.Timeseries{
+				Values: []float64{4, 4, 4},
+				TagSet: map[string]string{
+					"dc": "B",
+				},
+			},
+			api.Timeseries{
+				Values: []float64{0, 0, 1},
+				TagSet: map[string]string{
+					"dc": "C",
+				},
+			},
+		},
+	},
+	{
+		[]string{"dc", "env"},
+		meanAggregator{},
+		[]api.Timeseries{
+			api.Timeseries{
+				Values: []float64{0, 1, 2},
+				TagSet: map[string]string{
+					"dc":  "A",
+					"env": "staging",
+				},
+			},
+			api.Timeseries{
+				Values: []float64{-1.0 / 2.0, 1.0 / 2.0, 1.0},
+				TagSet: map[string]string{
+					"dc":  "A",
+					"env": "production",
+				},
+			},
+			api.Timeseries{
+				Values: []float64{4, 4, 4},
+				TagSet: map[string]string{
+					"dc":  "B",
+					"env": "staging",
+				},
+			},
+			api.Timeseries{
+				Values: []float64{2, 0, 0},
+				TagSet: map[string]string{
+					"dc":  "B",
+					"env": "production",
+				},
+			},
+			api.Timeseries{
+				Values: []float64{0, 0, 1},
+				TagSet: map[string]string{
+					"dc":  "C",
+					"env": "production",
+				},
+			},
+		},
+	},
+}
+
+func tagSetsEqual(leftSet api.TagSet, rightSet api.TagSet) bool {
+	for key, left := range leftSet {
+		right, ok := rightSet[key]
+		if !ok || left != right {
+			return false
+		}
+	}
+	return true
+}
+
+func Test_aggregateBy(t *testing.T) {
+
+	for _, testCase := range aggregatedTests {
+		aggregated, err := aggregateBy(testList, testCase.Aggregator, testCase.Tags)
+		if err != nil {
+			t.Error(err)
+		}
+		// Check that aggregated looks correct.
+		// There should be two series
+		if aggregated.Timerange != testList.Timerange {
+			t.Errorf("Expected aggregate's Timerange to be %+v but is %+v", testList.Timerange, aggregated.Timerange)
+			continue
+		}
+		if aggregated.Name != testList.Name {
+			t.Errorf("Expected aggregate's Name to be %s but is %s", testList.Name, aggregated.Name)
+			continue
+		}
+		if len(aggregated.Series) != len(testCase.Results) {
+			t.Errorf("Expected %d series in aggregation result but found %d", len(testCase.Results), len(aggregated.Series))
+			continue
+		}
+		// Lastly, we have to check that the values are correct.
+		// First, check that an aggregated series corresponding to each correct tagset:
+		for _, series := range testCase.Results {
+			found := false
+			for _, aggregate := range aggregated.Series {
+				if tagSetsEqual(series.TagSet, aggregate.TagSet) {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Fatalf("Expected to find series corresponding to %+v but could not in %+v", series.TagSet, aggregated)
+			}
+		}
+		// Next, each series will do the reverse-lookup and check that its values match the expected results.
+		// (It is neccesary to check both ways [see above] to ensure that the result doesn't contain just one of the series repeatedly)
+		for _, aggregate := range aggregated.Series {
+			// Any of the testCase results which it matches are candidates
+			for _, correct := range testCase.Results {
+				if tagSetsEqual(aggregate.TagSet, correct.TagSet) {
+					// Compare their values
+					for i := range aggregate.Values {
+						if math.Abs(aggregate.Values[i]-correct.Values[i]) > epsilon {
+							t.Errorf("For tagset %+v, result %+v does not match expected %+v", correct.TagSet, aggregate.Values, correct.Values)
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+
 }
