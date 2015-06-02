ab9821a652e85bbed4f9595e9cdffdbecd76fa36
Nathan Fenner <nathanf@squareup.com>
2015-06-02 10:56:25 -0700

    remove debug messages and add helpful comments and gofmt on generated code

--- build_verify.sh
+++ build_verify.sh
@@ -50,17 +50,18 @@ fi
 
 before=$(cat ./query/language.peg.go)
 ./query/build.sh
+# we have to reformat in case peg.go produces unformatted code
+gofmt -w ./query/language.peg.go
 after=$(cat ./query/language.peg.go)
 
 if [ "$before" != "$after" ]
 then
 	echo "FAIL: LANGUAGE .GO FILE IS NOT UP TO DATE"
 	echo "THERE WERE CHANGES TO query/language.peg AND NO CHANGES TO query/language.peg.go"
+	echo "Make sure you ran the build file, and that your version of peg is up to date."
+	echo "To get the latest version of peg, run:"
+	echo "> go get -u github.com/pointlander/peg"
 	fails="fails"
-	echo "======="
-	echo "$before"
-	echo "======="
-	echo "$after"
 fi
 
 if [ $fails ]
