7bd849597c5fae1f98e017662bdcf1fbbf0f9dd9
Nathan Fenner <nathanf@squareup.com>
2015-06-11 13:55:09 -0700

    cleanup

--- main/static/script.js
+++ main/static/script.js
@@ -47,18 +47,15 @@ function resultUpdate(object) {
 	var table = [labels];
 	// Next, add each row.
 
-	var startTime = object.body[0].timerange.start;
-	var endTime = object.body[0].timerange.end;
-	var resolution = object.body[0].timerange.resolution;
 
+	var timerange = object.body[0].timerange;
 	for (var t = 0; t < series[0].values.length; t++) {
-		var row = [new Date(startTime + resolution * t)];
+		var row = [new Date(timerange.start + timerange.resolution * t)];
 		for (var i = 0; i < series.length; i++) {
 			row.push(series[i].values[t] || 0);
 		}
 		table.push(row);
 	}
-	console.log(table);
 	var trueTable = google.visualization.arrayToDataTable(table);
 	var options = {
 		title: "Select Result",
@@ -68,41 +65,3 @@ function resultUpdate(object) {
 	var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 	chart.draw(trueTable, options);
 }
-
-/*
-function resultUpdate(value) {
-	if (chart) {
-		chart.destroy();
-		chart = null;
-	}
-	if (value && value.name == "select" && value.body && value.body.length && value.body[0].series && value.body[0].series.length) {
-		var datasets = [];
-		for (var i = 0; i < value.body.length; i++) {
-			var list = value.body[i];
-			for (var j = 0; j < list.series.length; j++) {
-				var series = list.series[j];
-				var seriesData = {
-					data:[],
-					label: "D" + datasets.length,
-		            strokeColor: "rgba(151,187,205,1)",
-				};
-				for (var t = 0; t < series.values.length; t++) {
-					seriesData.data[t] = series.values[t];
-				}
-				datasets.push(seriesData);
-			}
-		}
-		for (var i = 0; i < datasets.length; i++) {
-			datasets[i].pointColor = datasets[i].strokeColor = "hsl(" + Math.floor(360 * i / datasets.length) + ",50%,65%)";
-		}
-
-		var labels = [];
-		for (var i = 0; i < value.body[0].series[0].values.length; i++) {
-			labels[i] = i + "X";
-		}
-
-		chart = new Chart(ctx).Line({datasets:datasets,labels:labels}, {datasetFill:false, bezierCurve: false, pointDot:false});
-	}
-}
-
-*/
