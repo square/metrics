e3bcff88f30367aa72025e651d9aad26e7afcefb
Nathan Fenner <nathanf@squareup.com>
2015-06-12 16:45:25 -0700

    add messages for loading and rendering waits

--- main/static/script.js
+++ main/static/script.js
@@ -6,22 +6,16 @@ var chartsReady = false;
 
 var debounce = false;
 
-module.controller("mainCtrl", function($scope, $http) {
+module.controller("mainCtrl", function($scope, $http, $q) {
 	$scope.queryResult = "";
 	$scope.queryText = "";
+	$scope.launchedQuery = null;
+	// Triggers when the button is clicked.
 	$scope.onSubmitQuery = function() {
 		debounce = true;
 		window.location.hash = "#" + encodeURIComponent($scope.queryText);
-		$http.get('/query', {params:{query: $scope.queryText}}).
-		  success(function(data, status, headers, config) {
-		    $scope.queryResult = data;
-		  }).
-		  error(function(data, status, headers, config) {
-		    $scope.queryResult = data;
-		  });
+		launchRequest($scope.queryText);
 	};
-
-	$scope.$watch("queryResult", resultUpdate);
 
 	function readHash() {
 		if (debounce) {
@@ -35,8 +29,86 @@ module.controller("mainCtrl", function($scope, $http) {
 			// The remainder is the query to perform.
 			// Decode it (if neccesarry).
 			$scope.queryText = decodeURIComponent(urlQuery);
-			$scope.onSubmitQuery();
 		}
+		launchRequest($scope.queryText);
+	}
+
+	function cancelQuery() {
+		if ($scope.launchedQuery === null) {
+			return;
+		}
+		$scope.launchedQuery.canceler.resolve();
+		$scope.launchedQuery = null;
+	}
+
+	function launchRequest(query) {
+		cancelQuery();
+		var canceler = $q.defer();
+		var request = $http.get('/query', {
+			params:{query: query},
+			timeout:canceler // This allows the query to be canceled.
+		}).success(function(data, status, headers, config) {
+			$scope.queryResult = data;
+			receive(data);
+		}).error(function(data, status, headers, config) {
+			$scope.queryResult = data;
+			receive(data);
+		});
+		// Save the launched query so that it can be canceled later.
+		$scope.launchedQuery = {request: request, canceler:canceler};
+
+	}
+
+	$scope.chartWaiting = false;
+
+	function receive(object) {
+		$scope.launchedQuery = null;
+		if (!chartsReady) {
+			setTimeout(function(){receive(object);}, 10);
+			return;
+		}
+		if (!(object && object.name == "select" && object.body && object.body.length && object.body[0].series && object.body[0].series.length && object.body[0].timerange)) {
+			return;
+		}
+		var series = [];
+		for (var i = 0; i < object.body.length; i++) {
+			// Each of these is a list of series
+			for (var j = 0; j < object.body[i].series.length; j++) {
+				series.push(object.body[i].series[j]);
+			}
+		}
+		var labels = ["Time"];
+		for (var i = 0; i < series.length; i++) {
+			var label = "";
+			for (var k in series[i].tagset) {
+				label += k + ": " + series[i].tagset[k] + " "; 
+			}
+			labels.push(label);
+		}
+		var table = [labels];
+		// Next, add each row.
+
+		var timerange = object.body[0].timerange;
+		for (var t = 0; t < series[0].values.length; t++) {
+			var row = [dateFromIndex(t, timerange)];
+			for (var i = 0; i < series.length; i++) {
+				row.push(series[i].values[t] || NaN);
+			}
+			table.push(row);
+		}
+		var dataTable = google.visualization.arrayToDataTable(table);
+		var options = {
+			title: "Select Result",
+			legend: {position: "bottom"},
+			chartArea: {left: "5%", width:"90%"}
+		}
+		$scope.chartWaiting = true;
+		$scope.chart = new google.visualization.LineChart(document.getElementById('chart-div'));
+		google.visualization.events.addListener($scope.chart, 'ready', function() {
+			$scope.chartWaiting = false;
+			$scope.$apply(); // Updating to tell Angular that chartWaiting has changed.
+		});
+		setTimeout(function(){$scope.chart.draw(dataTable, options)}, 1);
 	}
 
 	readHash(); // Read the current hash (if present).
@@ -44,9 +116,6 @@ module.controller("mainCtrl", function($scope, $http) {
 	// Whenever the user changes the hash (such as pasting in a new URL), call readHash() again.
 	window.addEventListener("hashchange", readHash, false);
 });
-
-var chart;
-
 
 function onload() {
 	google.load('visualization', '1.0', {'packages':['corechart']});
@@ -57,50 +126,5 @@ function dateFromIndex(index, timerange) {
 	return new Date(timerange.start + timerange.resolution * index);
 }
 
+onload();
 
-
-function resultUpdate(object) {
-	if (!chartsReady) {
-		// Ask again in 30ms.
-		setTimeout(function(){resultUpdate(object);}, 10);
-		return;
-	}
-	if (!(object && object.name == "select" && object.body && object.body.length && object.body[0].series && object.body[0].series.length && object.body[0].timerange)) {
-		return;
-	}
-	var series = [];
-	for (var i = 0; i < object.body.length; i++) {
-		// Each of these is a list of series
-		for (var j = 0; j < object.body[i].series.length; j++) {
-			series.push(object.body[i].series[j]);
-		}
-	}
-	var labels = ["Time"];
-	for (var i = 0; i < series.length; i++) {
-		var label = "";
-		for (var k in series[i].tagset) {
-			label += k + ": " + series[i].tagset[k] + " "; 
-		}
-		labels.push(label);
-	}
-	var table = [labels];
-	// Next, add each row.
-
-	var timerange = object.body[0].timerange;
-	for (var t = 0; t < series[0].values.length; t++) {
-		var row = [dateFromIndex(t, timerange)];
-		for (var i = 0; i < series.length; i++) {
-			row.push(series[i].values[t] || NaN);
-		}
-		table.push(row);
-	}
-	var dataTable = google.visualization.arrayToDataTable(table);
-	var options = {
-		title: "Select Result",
-		legend: {position: "bottom"},
-		chartArea: {left: "5%", width:"90%"}
-	}
-
-	chart = new google.visualization.LineChart(document.getElementById('chart-div'));
-	setTimeout(function(){chart.draw(dataTable, options)}, 1);
-}
