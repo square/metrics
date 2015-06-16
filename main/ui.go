021981d505c03c68869fd018cd68fb9e9e9b5600
Nathan Fenner <nathanf@squareup.com>
2015-06-16 11:02:42 -0700

    add limit to execution context

--- main/ui.go
+++ main/ui.go
@@ -33,5 +33,5 @@ func main() {
 	apiInstance := common.NewAPI(config.API)
 	blueflood := blueflood.NewBlueflood(config.Blueflood)
 	backend := backend.NewSequentialMultiBackend(blueflood)
-	ui.Main(config.UIConfig, query.ExecutionContext{API: apiInstance, Backend: backend})
+	ui.Main(config.UIConfig, query.ExecutionContext{API: apiInstance, Backend: backend, Limit: 1000})
 }
