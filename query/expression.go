5297cfedf9716774f4ea1eef5654e71b64c040b2
Nathan Fenner <nathanf@squareup.com>
2015-06-16 13:50:35 -0700

    add timeshift registration

--- query/expression.go
+++ query/expression.go
@@ -37,12 +37,13 @@ func init() {
 	// Transformations
 	MustRegister(MakeTransformMetricFunction("transform.derivative", 0, transformTable["transform.derivative"]))
 	MustRegister(MakeTransformMetricFunction("transform.integral", 0, transformTable["transform.integral"]))
-
 	MustRegister(MakeTransformMetricFunction("transform.rate", 0, transformTable["transform.rate"]))
 	MustRegister(MakeTransformMetricFunction("transform.cumulative", 0, transformTable["transform.cumulative"]))
 	MustRegister(MakeTransformMetricFunction("transform.moving_average", 0, transformTable["transform.moving_average"]))
 	MustRegister(MakeTransformMetricFunction("transform.default", 0, transformTable["transform.default"]))
 	MustRegister(MakeTransformMetricFunction("transform.abs", 0, transformTable["transform.abs"]))
+	// Timeshift
+	MustRegister(TimeshiftFunction)
 }
 
 // EvaluationContext is the central piece of logic, providing
