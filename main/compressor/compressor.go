7a0844afa846c17bb7d933b6dfed5ba5ffcc6f4b
Alex Coomans <alexc@squareup.com>
2016-04-07 11:12:03 -0700

    Make sure all files have the same license

--- main/compressor/compressor.go
+++ main/compressor/compressor.go
@@ -1,4 +1,4 @@
-// Copyright 2015 Square Inc.
+// Copyright 2015 - 2016 Square Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,10 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package main
 
 import (
 	"fmt"
+
 	"github.com/square/metrics/compress"
 )
 
