1f58c30236182f844b4ac6156b02924a990fc4a4
Nathan Fenner <nathanfenner@squareup.com>
2016-06-30 15:19:09 -0700

    Update memoize.go

--- function/memoize.go
+++ function/memoize.go
@@ -90,8 +90,7 @@ func (m memoizedExpression) ExpressionString(mode DescriptionMode) string {
 	return m.Expression.ExpressionString(mode)
 }
 
-////////
-
+// memoization map holds a collection of memoization points.
 type memoizationMap struct {
 	sync.Mutex
 	Map map[contextIdentity]*memoization
